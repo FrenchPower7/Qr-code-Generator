41a52c44ebea8d95633ac23f26bea245d076b26b
French <152695796+FrenchPower7@users.noreply.github.com>
2024-09-19 20:30:21 +0200

    Update QRcode-creator.py

--- QRcode-creator.py
+++ QRcode-creator.py
@@ -1,20 +1,19 @@
 import qrcode
 import os
 
-# Chemin où l'image sera enregistrée (jsp pk mais il me le mets à la racine ce fdpdf)
-chemin_image = "C:/Users/Ethan B/OneDrive - Reseau-GES/Bureau/test"
+# Here is the folder where its save
+chemin_image = "C:/Users/"
 
-# Créer un QR code avec le texte 
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-qr.add_data("J'ai ton ip 🥺")
+qr.add_data("Put right here the text you want")
 qr.make(fit=True)
 
-# Créer une image QR code (avec les couleur de base(oui oui tu mets "red" à la place de black ça marche))
+# Here is the clasic color
 # color1 = "black"
 # color2 = "white"
 
@@ -22,8 +21,8 @@ color1 = "green"
 color2 = "cyan"
 img = qr.make_image(fill_color=color1, back_color=color2)
 
-# Sauvegarder l'image QR code
-nom_image = "qrcode_ilestbo.png"
+# Save the image as a png
+nom_image = "qrcode.png"
 chemin_complet_image = os.path.join(chemin_image, nom_image)
 img.save(chemin_complet_image)
 
